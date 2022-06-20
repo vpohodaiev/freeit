5bb86aaf01eda8ee82047fe16f2c47f447bed338
Volodymyr Pohodaiev <volodymyr.pohodaiev@dataart.com>
2022-06-20 11:10:35 +0300

    homework 1 part 2added

---
+++ part_1/src/com/freeit/vpohodaiev/lesson2/homework_task1.java
@@ -0,0 +1,20 @@
+package com.freeit.vpohodaiev.lesson2;
+
+public class homework_task1 {
+    public static void main (String [] args) {
+        int x = 5;
+        int y = 8;
+        if (x<y)
+        {
+            System.out.println("x less than y");
+        }
+        else if (x>y)
+        {
+            System.out.println("x more than y");
+        }
+        else
+        {
+            System.out.println("x equals y");
+        }
+    }
+}
