9169a2201463aa45d58a786d0cb6a168ecf71bf5
vpohodaiev <pogodaev.vova@gmail.com>
2022-06-23 13:33:56 +0300

    homework 1 part 2: code formatted, scanner class added

--- part_1/src/com/freeit/vpohodaiev/lesson2/homework_task1.java
+++ part_1/src/com/freeit/vpohodaiev/lesson2/homework_task1.java
@@ -1,9 +1,16 @@
 package com.freeit.vpohodaiev.lesson2;
 
+import java.util.Scanner;
+
 public class homework_task1 {
     public static void main (String [] args) {
-        int x = 5;
-        int y = 8;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Fill in x number and click on Enter");
+        int x;
+        x = sc.nextInt();
+        System.out.println("Fill in y number and click on Enter");
+        int y ;
+        y = sc.nextInt();
         if (x<y)
         {
             System.out.println("x less than y");
